1109cd5e9822afda1d22c539363b8c27429b197e
阿森 <zhengxs2018@foxmail.com>
2023-12-23 14:32:02 +0800

    style: 代码格式化

--- examples/wechaty.ts
+++ examples/wechaty.ts
@@ -1,18 +1,16 @@
-
 import path from 'node:path';
 
 import { PuppetDingTalk } from '@zhengxs/wechaty-puppet-dingtalk';
 import { FileBox } from 'file-box';
 import { WechatyBuilder } from 'wechaty';
 
-
 const puppet = new PuppetDingTalk({
   clientId: process.env.DINGTALK_CLIENT_ID,
   clientSecret: process.env.DINGTALK_CLIENT_SECRET,
-})
+});
 
 const bot = WechatyBuilder.build({
-  puppet
+  puppet,
 });
 
 bot.on('message', async msg => {
@@ -24,17 +22,17 @@ bot.on('message', async msg => {
     case MessageTypes.Video:
     case MessageTypes.Image: {
       const fileBox = await msg.toFileBox();
-      const saveTo = path.resolve(__dirname, 'data', fileBox.name)
+      const saveTo = path.resolve(__dirname, 'data', fileBox.name);
 
-      await fileBox.toFile(saveTo)
+      await fileBox.toFile(saveTo);
       await msg.say(`File saved to: ${saveTo}`);
       break;
     }
 
     case MessageTypes.Text: {
       if (msg.text() === 'ding') {
-        await msg.say('dong')
-        break
+        await msg.say('dong');
+        break;
       }
 
       await msg.say(FileBox.fromUrl('https://www.baidu.com/img/PCfb_5bf082d29588c07f842ccde3f97243ea.png'));
